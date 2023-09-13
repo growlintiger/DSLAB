3e85f8745080a090fbec382c031c678208f40554
Alex Binu <103043664+growlintiger@users.noreply.github.com>
2023-09-13 15:03:56 +0530

    Update Linked list(menu based).c

--- Linked list(menu based).c
+++ Linked list(menu based).c
@@ -114,11 +114,41 @@ void  insertAtPosition()
          
      }
 }  
-    
+
+void  insertAfterElement()
+{
+    int ele1,ele2;
+    struct node *temp;
+    struct node ptr = (struct node)malloc(sizeof(struct node));
+    printf("Enter the element after which you want to insert\n");
+    scanf("%d",&ele1);
+    printf("Enter the element you want to insert\n");
+    scanf("%d",&ele2);
+
+    ptr->data=ele2;
+    ptr->next=NULL;
+
+    temp=start;
+    if(start==NULL)
+        printf("List is empty");
+    else
+    {
+        while(temp->data!=ele1)
+            temp=temp->next;
+        if(temp==NULL)
+            printf("Element not found");
+        else
+        {
+            ptr->next=temp->next;
+            temp->next=ptr;
+        }
+    }
+}
+
 int main()
 {   
-    printf("\n 1  create the list\n 2  For insertion at starting\n 3  For insertion at end\n 4  For insertion at any position \n 5 Traversal \n");
-    printf(" 6 To exit\n");
+    printf("\n 1  create the list\n 2  For insertion at starting\n 3  For insertion at end\n 4  For insertion at any position \n 5 Traversal \n6 Insert after element\n");
+    printf(" 7 To exit\n");
     int choice;
     while (1) 
     {
@@ -142,8 +172,10 @@ int main()
                 traversal();
                 break;
             case 6:
-                exit(1);
+                insertAfterElement();
                 break;
+            case 7:
+                exit(1);
             default:
                 printf("Incorrect Choice\n");
         }
